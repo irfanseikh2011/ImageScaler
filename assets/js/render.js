7e3a184a2c9d6ac0a56ab98dad5357484d4a006d
Irfan Sheikh <seikhirfan2011@gmail.com>
2020-06-18 13:29:14 +0530

    getting the information about the picture and the required size

--- assets/js/render.js
+++ assets/js/render.js
@@ -1,7 +1,23 @@
-const path = require('path');
-const os = require('os');
+const path = require('path')
+      const os = require('os')
+      const { ipcRenderer } = require('electron')
 
-document.getElementById('output-path').innerText = path.join(
-    os.homedir(),
-    'ImageScaler'
-)
+      const form = document.getElementById('image-form')
+      const slider = document.getElementById('slider')
+      const img = document.getElementById('img')
+
+      document.getElementById('output-path').innerText = path.join(
+        os.homedir(),
+        'imageScaler'
+      )
+
+      //   Onsubmit
+      form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const imgPath = img.files[0].path
+        const quality = slider.value
+
+        console.log(imgPath,quality);
+
+      })
